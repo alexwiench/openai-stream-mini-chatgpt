894f1d8c340bdb2d5768a874bdeaf7923ddcc9c2
Alex Wiench <dev@alexwiench.com>
2023-04-16 22:11:27 -0500

    update for chat completions

--- src/index.ts
+++ src/index.ts
@@ -1,120 +1,119 @@
-import type { CreateCompletionRequest } from "openai";
+import type { CreateChatCompletionRequest } from 'openai';
 
 export type OnTextCallback = (text: string) => Promise<void> | undefined;
 
-import { throttle } from "./throttle";
+import { throttle } from './throttle';
 
 interface StreamCompletionArgs {
-  apiKey: string;
-  args: CreateCompletionRequest;
-  onText: OnTextCallback;
-  throttleMs?: number;
+	apiKey: string;
+	args: CreateChatCompletionRequest;
+	onText: OnTextCallback;
+	throttleMs?: number;
 }
 
 /**
  * Stream a completion from OpenAI.
  */
 export const streamCompletion = async ({
-  apiKey,
-  args,
-  onText,
-  throttleMs,
+	apiKey,
+	args,
+	onText,
+	throttleMs,
 }: StreamCompletionArgs): Promise<string> => {
-  // throttle callback?
-  const handler = throttleMs ? throttle(throttleMs, onText, {}) : onText;
+	// throttle callback?
+	const handler = throttleMs ? throttle(throttleMs, onText, {}) : onText;
 
-  // stream the completion
-  return await _streamCompletion(apiKey, args, handler);
+	// stream the completion
+	return await _streamCompletion(apiKey, args, handler);
 };
 
 const _streamCompletion = async (
-  token: string,
-  args: CreateCompletionRequest,
-  onText: OnTextCallback
+	token: string,
+	args: CreateChatCompletionRequest,
+	onText: OnTextCallback
 ) => {
-  const response = await fetch("https://api.openai.com/v1/completions", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      ...args,
-      stream: true,
-    }),
-  });
-  if (!response.ok || !response.body) {
-    console.error("OpenAI stream failed", response);
-    console.error(await response.text());
-    throw new Error("OpenAI stream failed");
-  }
+	const response = await fetch('https://api.openai.com/v1/chat/completions', {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${token}`,
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			...args,
+			prompt: args.messages.map((message) => `${message.role}: ${message.content}`).join('\n'),
+			stream: true,
+		}),
+	});
+	if (!response.ok || !response.body) {
+		console.error('OpenAI stream failed', response);
+		console.error(await response.text());
+		throw new Error('OpenAI stream failed');
+	}
 
-  const decoder = new TextDecoder("utf8");
-  const reader = response.body.getReader();
+	const decoder = new TextDecoder('utf8');
+	const reader = response.body.getReader();
 
-  let fullText = "";
+	let fullText = '';
 
-  async function readMore() {
-    const { value, done } = await reader.read();
+	async function readMore() {
+		const { value, done } = await reader.read();
 
-    if (done) {
-      await onText(fullText);
-    } else {
-      const str = decoder.decode(value);
+		if (done) {
+			await onText(fullText);
+		} else {
+			const str = decoder.decode(value);
 
-      // split on newlines
-      const lines = str.split(/(\r\n|\r|\n)/g);
+			// split on newlines
+			const lines = str.split(/(\r\n|\r|\n)/g);
 
-      const beforeText = fullText;
+			const beforeText = fullText;
 
-      for (const line of lines) {
-        if (!line.trim()) {
-          continue;
-        }
+			for (const line of lines) {
+				if (!line.trim()) {
+					continue;
+				}
 
-        let prefix;
-        if (line.startsWith("data:")) {
-          prefix = "data:";
-        } else if (line.startsWith("delta:")) {
-          prefix = "delta:";
-        } else {
-          console.error("Unexpected response from OpenAI stream, line=", line);
-          throw new Error(
-            "Unexpected response from OpenAI stream, line=" + line
-          );
-        }
+				let prefix;
+				if (line.startsWith('data:')) {
+					prefix = 'data:';
+				} else if (line.startsWith('delta:')) {
+					prefix = 'delta:';
+				} else {
+					console.error('Unexpected response from OpenAI stream, line=', line);
+					throw new Error('Unexpected response from OpenAI stream, line=' + line);
+				}
 
-        const data = line.slice(prefix.length);
-        if (data.trim().startsWith("[DONE]")) {
-          return;
-        }
+				const data = line.slice(prefix.length);
+				if (data.trim().startsWith('[DONE]')) {
+					return;
+				}
 
-        let json;
-        try {
-          json = JSON.parse(data);
-        } catch (error) {
-          console.error("Unexpected response from OpenAI stream, data=", data);
-          throw error;
-        }
+				let json;
+				try {
+					json = JSON.parse(data);
+				} catch (error) {
+					console.error('Unexpected response from OpenAI stream, data=', data);
+					throw error;
+				}
 
-        if (json.content) {
-          fullText += json.content;
-        } else if (json.choices) {
-          fullText += json.choices[0].text;
-        } else {
-          console.warn("Unexpected response from OpenAI stream, json=", json);
-        }
-      }
+				if (json.content) {
+					fullText += json.content;
+				} else if (json.choices) {
+					fullText += json.choices[0].text;
+				} else {
+					console.warn('Unexpected response from OpenAI stream, json=', json);
+				}
+			}
 
-      if (beforeText !== fullText) {
-        await onText(fullText);
-      }
+			if (beforeText !== fullText) {
+				await onText(fullText);
+			}
 
-      await readMore();
-    }
-  }
+			await readMore();
+		}
+	}
 
-  await readMore();
+	await readMore();
 
-  return fullText;
+	return fullText;
 };
